4d8ee79e25ef068ca4db2a95892987ddfe71d2ee
suchitranarayandas <suchitranarayandas@gmail.com>
2025-05-13 22:43:50 +0530

    Create sw.js

---
+++ static/sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'app-cache-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/form',
+  '/upload_qr',
+  '/static/style.css',
+  '/static/logo.png',  // <-- Add any static assets your app uses
+];
+
+// Install event: caching files
+self.addEventListener('install', event => {
+  console.log('[ServiceWorker] Install');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('[ServiceWorker] Pre-caching offline files');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate event: cleanup old caches
+self.addEventListener('activate', event => {
+  console.log('[ServiceWorker] Activate');
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            console.log('[ServiceWorker] Removing old cache', key);
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch event: serve cached files if offline
+self.addEventListener('fetch', event => {
+  console.log('[ServiceWorker] Fetch', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
