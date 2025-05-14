8e1270df386e483376da64b952544cb36ab90217
suchitranarayandas <suchitranarayandas@gmail.com>
2025-05-14 07:35:33 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -99,7 +99,7 @@ async function saveFormDataLocally(request) {
     await tx.done;
     console.log('[ServiceWorker] Form data saved locally');
     
-    return new Response(JSON.stringify({ message: 'Form data saved locally, will retry later' }), {
+    return new Response(JSON.stringify({ message: 'Form data saved locally, will retry later' }), {status: 200,
   headers: { 'Content-Type': 'application/json' }
 });
   } catch (error) {
